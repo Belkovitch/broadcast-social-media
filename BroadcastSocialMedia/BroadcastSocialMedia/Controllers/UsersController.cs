8a2a11fccbb8c1e13fff85948c9dd452cd78b324
Johnny <johnny.andersson.91@gmail.com>
2024-10-25 15:12:58 +0200

    Added show Broadcast

--- BroadcastSocialMedia/BroadcastSocialMedia/Controllers/UsersController.cs
+++ BroadcastSocialMedia/BroadcastSocialMedia/Controllers/UsersController.cs
@@ -1,5 +1,7 @@
 ﻿using BroadcastSocialMedia.Data;
+using BroadcastSocialMedia.Models;
 using BroadcastSocialMedia.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,20 +10,43 @@ namespace BroadcastSocialMedia.Controllers
     public class UsersController : Controller
     {
         private readonly ApplicationDbContext _dbContext;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public UsersController(ApplicationDbContext dbContext)
+
+        public UsersController(ApplicationDbContext dbContext, UserManager<ApplicationUser> userManager)
         {
             _dbContext = dbContext;
+            _userManager = userManager;
         }
         public async Task<IActionResult> Index(UsersIndexViewModel viewModel)
         {
             if(viewModel.Search != null)
             {
-                var users = await _dbContext.Users.Where(u => u.Name.Contains(viewModel.Search))
+                var users = await _dbContext.Users
+                    .Where(u => u.Name.Contains(viewModel.Search))
                     .ToListAsync();
                 viewModel.Result = users;
             }
             
+            return View(viewModel);
+        }
+
+        [Route("/Users/{id}")]
+        public async Task<IActionResult> ShowUser(string id)
+        {
+            var broadcasts = await _dbContext.Broadcasts
+                .Where(b => b.User.Id == id)
+                .OrderByDescending(b => b.Published)
+                .ToListAsync();
+            var user = await _userManager.GetUserAsync(User);
+
+            var viewModel = new UsersShowUserViewModel()
+            {
+                Broadcasts = broadcasts,
+                User = user
+
+            };
+
             return View(viewModel);
         }
     }
