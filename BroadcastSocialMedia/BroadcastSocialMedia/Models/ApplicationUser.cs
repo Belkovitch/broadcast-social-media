80ba2903e810f075d887939f087a35108d5bd34c
Johnny <johnny.andersson.91@gmail.com>
2024-10-25 12:30:55 +0200

    Added name to user profile

---
+++ BroadcastSocialMedia/BroadcastSocialMedia/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BroadcastSocialMedia.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? Name { get; set; }
+        public string Email { get; set; }
+    }
+}
